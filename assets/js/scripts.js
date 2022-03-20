5af5ad2b446486ca4ac7ec274e5ae99e4c9b8509
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-20 12:36:01 +0300

    delivery input added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,20 @@
-var pizza = $("#pizza").val();
+$(document).ready(function() {
+    
+    var size = $("#size").val();
+    var pizza = $("#pizza").val();
+    var topping= $("#topping").val();
+
+    $("#blanks form").submit(function(event) {
+        var blanks = ["size", "crust", "topping"];
+
+
+        $("#order").show();
+
+        event.preventDefault();
+
+
+
+    });
+
+
+});
