bac088d69c429212d4bf58a15897ec81a055103d
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-20 20:01:35 +0300

    building js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,31 @@
 $(document).ready(function() {
     
-    var size = $("#size").val();
-    var pizza = $("#pizza").val();
-    var topping= $("#topping").val();
-
-    $("#blanks form").submit(function(event) {
-        var blanks = ["size", "crust", "topping"];
 
 
-        $("#order").show();
+$('#orderButton').click(function() {
+    var pizzaSize = $(".size option:selected").val();
+    var pizzaTopping = $(".toppings option:selected").val();
+    var pizzaCrust = $(".crust option:selected").val();
+    var total = parseInt(pizzaSize) + parseInt(pizzaTopping) + parseInt(pizzaCrust);
+    var order = 1;
+    var grandTotal = 0;
 
-        event.preventDefault();
+    $("#").show();
+    $("#addBtn").show();
+    $("#orderButton").hide();
 
+    $("#sizeData").html($(".size option:selected").text() + " - " + PizzaSize);
+    $("#toppingsData").html($(".toppings option:selected").text() + " - " + pizzaToppings);
+    $("#crustData").html($(".crust option:selected").text() + " - " + pizzaCrust);
+    $("#totalAmount").html(total);
 
+    function Pizza(size, toppings, crust, total, orderNo) {
+      this.size = size;
+      this.toppings = toppings;
+      this.crust = crust;
+      this.total = total;
+      this.orderNo = orderNo;
+    }
 
-    });
-
-
-});
+    
+}
