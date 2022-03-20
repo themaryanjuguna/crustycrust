fdd8f981f763036144aa214032b06a69b7b58287
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-21 00:47:04 +0300

    table is appending

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,77 @@
 $(document).ready(function() {
     
 
+  $('.btn.orderbutton').click(function() {
+      var pizzaSize = $(".size option:selected").val();
+      var pizzaTopping = $(".topping option:selected").val();
+      var pizzaCrust = $(".crust option:selected").val();
+      var total = parseInt(pizzaSize) + parseInt(pizzaTopping) + parseInt(pizzaCrust);
+      var order = 1;
+      var totalAmount = 0;
 
-$('#orderButton').click(function() {
-    var pizzaSize = $(".size option:selected").val();
-    var pizzaTopping = $(".toppings option:selected").val();
-    var pizzaCrust = $(".crust option:selected").val();
-    var total = parseInt(pizzaSize) + parseInt(pizzaTopping) + parseInt(pizzaCrust);
-    var order = 1;
-    var grandTotal = 0;
+      $("table").show();
+      $(".add-pizza").show();
+      $(".orderbutton").hide();
 
-    $("#").show();
-    $("#addBtn").show();
-    $("#orderButton").hide();
+      $("#size").html($(".size option:selected").text() + " - " + pizzaSize);
+      $("#topping").html($(".topping option:selected").text() + " - " + pizzaTopping);
+      $("#crust").html($(".crust option:selected").text() + " - " + pizzaCrust);
+      $("#total").html(total);
 
-    $("#sizeData").html($(".size option:selected").text() + " - " + PizzaSize);
-    $("#toppingsData").html($(".toppings option:selected").text() + " - " + pizzaToppings);
-    $("#crustData").html($(".crust option:selected").text() + " - " + pizzaCrust);
-    $("#totalAmount").html(total);
+      function Pizza(size, topping, crust, total, orderNo) {
+        this.size = size;
+        this.topping = topping;
+        this.crust = crust;
+        this.total = total;
+        this.orderNo = orderNo;
+      }
 
-    function Pizza(size, toppings, crust, total, orderNo) {
-      this.size = size;
-      this.toppings = toppings;
-      this.crust = crust;
-      this.total = total;
-      this.orderNo = orderNo;
-    }
 
+      $('.btn.add-pizza').click(function() {
+          var pizzaSize = $(".size option:selected").val();
+          var pizzaTopping = $(".topping option:selected").val();
+          var pizzaCrust = $(".crust option:selected").val();
+          var total = parseInt(pizzaSize) + parseInt(pizzaTopping) + parseInt(pizzaCrust);
+          order = order + 1;
+          totalAmount = totalAmount + total;
     
-}
+    
+          var newPizza = new Pizza(pizzaSize, pizzaTopping, pizzaCrust, total, order);
+    
+          var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="topping">' + $(".topping option:selected").text() + " - " + newPizza.topping + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+    
+          $(".pizzaorder").append(newRow);
+        });
+
+        $(".btn.checkout").click(function() {
+          $(".btn.add-pizza").hide();
+          $(".btn.checkout").hide();
+          $(".other-options").show();
+          $(".yesbutton").show();
+          $(".nobutton").show();
+          $(".other-options .deliveryaddress").hide();
+          totalAmount = totalAmount + total;
+
+          $(".other-options fs-4").html(totalAmount);
+        });
+
+
+        $(".yesbutton").click(function() {
+          $(".other-options fs-4").hide();
+          $(".btn.yes").hide();
+          $(".btn.no").hide();
+          $(".other-options .deliveryaddress").show();
+          $(".other-options fs-4").html(totalAmount + 200);
+        });
+
+        $(".nobutton").click(function() {
+          $(".other-options h5").hide();
+          $(".btn.yes").hide();
+          $(".btn.no").hide();
+          $(".other-options .deliveryaddress").show();
+        });
+
+    });
+  
+ });
+
